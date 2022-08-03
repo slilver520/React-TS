3ff443532db0d350a251009cfec4ae24c1481f11
slilver520 <slilver520@naver.com>
2022-08-03 15:03:02 +0900

    Context API 활용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import Counter from './Counter';
+import MyForm from './MyForm';
+import ReducerSample from './Reducer';
 
 
 const App: React.FC = () => {
-return <Counter />;
+  const onSubmit = (form: {name: string; description: string}) => {
+    console.log(form);
+  }
+  return (
+    <div>
+      <Counter/>
+      <MyForm onSubmit={onSubmit}></MyForm>
+      <ReducerSample/>
+    </div>
+  );
 }
 
 export default App;
